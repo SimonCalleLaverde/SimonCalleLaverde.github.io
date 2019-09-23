d00d1ac19021e5aea3dcb31eae5637a474331a7e
Simkyverde88 <kyverde88@gmail.com>
2019-09-23 16:29:00 -0500

    Removing "#loader" dots animation

--- assets/js/custom_javascripts.js
+++ assets/js/custom_javascripts.js
@@ -141,28 +141,28 @@ $(function() {
   });
 });
 
-// Loader Dots | As Per: https://codepen.io/WhiteWolfWizard/pen/vorqj
-$(function() {
-  var $loader = $('#loader'),
-    max = 75, speed = 500,
-    char = '<i>.</i>', count = 0,
-    dots = function(){
-      if ( count <= max ) {
-        count++;
-        for ( var i = 0; i < 1; i++ ) {
-          $loader.append(char);
-          $loader.find('i').fadeIn(speed);
-        }
-      } else {
-        clearInterval(dots);
-      }
-      // COLOR FUN * OPTIONAL *
-      /*
-      $('#loader i').each(function(i){
-        var hue = 10 * i;
-        $(this).css({ color: 'hsl('+hue+',75%,50%)' });
-      });
-      */
-    };
-  setInterval(dots,speed/2);
-});
+// // Loader Dots | As Per: https://codepen.io/WhiteWolfWizard/pen/vorqj
+// $(function() {
+//   var $loader = $('#loader'),
+//     max = 75, speed = 500,
+//     char = '<i>.</i>', count = 0,
+//     dots = function(){
+//       if ( count <= max ) {
+//         count++;
+//         for ( var i = 0; i < 1; i++ ) {
+//           $loader.append(char);
+//           $loader.find('i').fadeIn(speed);
+//         }
+//       } else {
+//         clearInterval(dots);
+//       }
+//       // COLOR FUN * OPTIONAL *
+//       /*
+//       $('#loader i').each(function(i){
+//         var hue = 10 * i;
+//         $(this).css({ color: 'hsl('+hue+',75%,50%)' });
+//       });
+//       */
+//     };
+//   setInterval(dots,speed/2);
+// });
