f42a56423c3162454d7eeab32ef2179659b0dbea
Simon Calle Laverde <kyverde88@gmail.com>
2020-10-24 20:10:57 -0500

    *

--- assets/js/live_tests.js
+++ assets/js/live_tests.js
@@ -5,8 +5,8 @@ container.addEventListener("mousemove", (e) => {
 
 	//console.log(e.pageX, e.pageY);
 
-	let xAxis = (window.innerWidth / 2 - e.pageX) / 20;
-	let yAxis = (window.innerHeight / 2 - e.pageY) / 20;
+	let xAxis = (window.innerWidth / 2 - e.pageX) / 15;
+	let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
 
 	card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
 
