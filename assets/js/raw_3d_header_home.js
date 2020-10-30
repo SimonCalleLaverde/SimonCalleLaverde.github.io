6749016f9a6c3d72fb0d833d8f22db69fafd9498
Simon Calle Laverde <kyverde88@gmail.com>
2020-10-30 11:39:38 -0500

    *

---
+++ assets/js/raw_3d_header_home.js
@@ -0,0 +1,26 @@
+const container = document.querySelector(".home-hello-header .container");
+const card = document.querySelector(".header-3d");
+
+container.addEventListener("mousemove", (e) => {
+
+  //console.log(e.pageX, e.pageY);
+
+  let xAxis = (window.innerWidth / 2 - e.pageX) / 15;
+  let yAxis = (window.innerHeight / 2 - e.pageY) / 15;
+
+  card.style.transform = `rotateY(${xAxis}deg) rotateX(${yAxis}deg)`;
+
+});
+
+// Animate In
+container.addEventListener("mouseenter", (e) => {
+  card.style.transition = "all 0.5s ease-out";//card.style.transition = "none";
+});
+
+// Animate Out
+container.addEventListener("mouseleave", (e) => {
+  card.style.transform = `rotateY(0deg) rotateX(0deg)`;
+  card.style.transition = "all 0.5s ease-in";//cubic-bezier(0.215, 0.610, 0.355, 1.5)
+});
+
+// 28:07
