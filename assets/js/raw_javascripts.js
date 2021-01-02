a0fc881fcea514f720b45fcf78cfa957e827228c
Simon Calle Laverde <kyverde88@gmail.com>
2021-01-02 12:48:24 -0500

    Last nav desktop changes.

--- assets/js/raw_javascripts.js
+++ assets/js/raw_javascripts.js
@@ -125,7 +125,7 @@ if ('addEventListener' in document) {//Github
 // }, false);
 
 // Init Rellax
-$(window).ready(function() {//onload//$(document).ready(function() {//$(function() {
+$(document).ready(function() {//$(window).ready(function() {//onload//$(function() {
   // Accepts any class name
   var rellax = new Rellax('.rellax', {
     center:true
